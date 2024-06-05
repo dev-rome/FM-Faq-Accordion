c5892bf1051ffba27c5ccc27458cd13f252e4b08
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-04 23:10:27 -0400

    add logic to show accordion answers

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const questionEl = document.querySelectorAll('.question');
+const answerEl = document.querySelectorAll('.question-wrapper p');
+
+questionEl.forEach((question) => {
+    question.addEventListener("click", function () {
+        answerEl.forEach((answer) => {
+            if (answer !== question.nextElementSibling) {
+                answer.style.display = 'none';
+            }
+        });
+
+        const answer = question.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
