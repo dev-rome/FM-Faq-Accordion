fde5496f1d6d1f78a980e539231570e9a08e06aa
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-04 23:56:41 -0400

    switch icons when answers are display and hidden

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ questionEl.forEach((question) => {
         answerEl.forEach((answer) => {
             if (answer !== question.nextElementSibling) {
                 answer.style.display = 'none';
+                const icon = answer.previousElementSibling.querySelector('img');
+                if (icon) {
+                    icon.src = './assets/images/icon-plus.svg';
+                }
             }
         });
 
         const answer = question.nextElementSibling;
+        const icon = question.querySelector('img');
         if (answer.style.display === 'block') {
             answer.style.display = 'none';
+            icon.src = './assets/images/icon-plus.svg';
         } else {
             answer.style.display = 'block';
+            icon.src = './assets/images/icon-minus.svg';
         }
     });
 });
